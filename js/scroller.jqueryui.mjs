3147817e56fb98f38e5178daac3845def8e5c6dd
Allan Jardine <allan@datatables.net>
2022-10-10 19:50:54 +0000

    Add support for mjs files (ES Modules)

---
+++ js/scroller.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jQuery UI styling wrapper for Scroller
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-scroller';
+
+
+
+
+export default DataTable;
