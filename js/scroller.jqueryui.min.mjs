531611d5a50414797fa09765cbd921342d8413a8
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:14:30 +0000

    b343af8c59e921a068a47d3ce933db0768114fee
    Sync to source repo @b343af8c59e921a068a47d3ce933db0768114fee

--- js/scroller.jqueryui.min.mjs
+++ js/scroller.jqueryui.min.mjs
@@ -1,1 +1,4 @@
+/*! jQuery UI styling wrapper for Scroller
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-ju";import DataTable from"datatables.net-scroller";export default DataTable;
