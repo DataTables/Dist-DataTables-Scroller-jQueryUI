246d9967fe3c5b0b1e860f5edf4ea48e06f2e70f
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-09 12:17:00 +0100

    c15a3fdf2a72f6050fb461101a4f61dd6af9b65d Dev: Code formatted with Pretter More
    6dfe901befd10e0a982899edfa659d764006a94d Dev: Code formatted with Pretter More
    
    Last commit was just the version number!
    
    Sync to source repo @6dfe901befd10e0a982899edfa659d764006a94d

--- js/scroller.jqueryui.min.mjs
+++ js/scroller.jqueryui.min.mjs
@@ -1,4 +1,4 @@
 /*! jQuery UI styling wrapper for Scroller
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-jqui";import Scroller from"datatables.net-scroller";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-jqui";import Scroller from"datatables.net-scroller";let $=jQuery;export default DataTable;
