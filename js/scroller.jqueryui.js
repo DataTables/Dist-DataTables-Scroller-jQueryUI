8835f910d220ce16880640c7c71e3a4656a9def0
dtbuild <dtbuild@rigil>
2026-09-22 10:54:04 +0000

    f3a7546ea9c4a67be4402f7fee4db82f2cb6ac81 Release 3.1.0
    Sync to source repo @f3a7546ea9c4a67be4402f7fee4db82f2cb6ac81

--- js/scroller.jqueryui.js
+++ js/scroller.jqueryui.js
@@ -1,4 +1,4 @@
-/*! Scroller jQuery UI styling 3.0.0 for DataTables
+/*! Scroller jQuery UI styling 3.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
--- js/scroller.jqueryui.min.mjs
+++ js/scroller.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! Scroller jQuery UI styling 3.0.0 for DataTables
+/*! Scroller jQuery UI styling 3.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import Scroller from"datatables.net-scroller";export default DataTable;
